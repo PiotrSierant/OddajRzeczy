5371d65106fab6a9729ffb36fc32d6fa14c6772e
PiotrSierant <piotr.sierant96@gmail.com>
2022-09-08 15:43:04 +0200

    header part I

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
-import styles from "./Home.scss";
+import styles from "./Home.module.scss";
 import { Navbar } from "../Navbar/Navbar";
 export function Home() {
   return (
     <header>
       <Navbar />
+      <div className={styles.hero} />
     </header>
   );
 }
