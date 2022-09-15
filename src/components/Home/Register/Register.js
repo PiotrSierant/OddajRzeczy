8220997345b2a391de022939733473e992eabcc7
PiotrSierant <piotr.sierant96@gmail.com>
2022-09-15 08:20:11 +0200

    register form

--- src/components/Home/Register/Register.js
+++ src/components/Home/Register/Register.js
@@ -1,13 +1,14 @@
 import React from "react";
 import styles from "./Register.module.scss";
 import { Navbar } from "../../Navbar/Navbar";
+import { RegisterDetails } from "./RegisterDetails";
 
 export function Register() {
   return (
     <div className={styles.container}>
       <header>
         <Navbar />
-        <h1>Register</h1>
+        <RegisterDetails />
       </header>
     </div>
   );
