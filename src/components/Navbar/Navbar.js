696b98bc99424afced44f33df066271cf992095e
PiotrSierant <piotr.sierant96@gmail.com>
2022-09-19 15:49:55 +0200

    adding container navbar(max 1366px)

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { MobileMenu } from "./MobileMenu";
 import { Menu } from "./Menu";
+import styles from "./Navbar.module.scss";
 
 export function Navbar({ isLogged, setIsLogged }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,14 +10,16 @@ export function Navbar({ isLogged, setIsLogged }) {
   };
 
   return (
-    <nav>
-      <MobileMenu isOpen={isOpen} handleClickMenu={handleClickMenu} />
-      <Menu
-        isOpen={isOpen}
-        handleClickMenu={handleClickMenu}
-        isLogged={isLogged}
-        setIsLogged={setIsLogged}
-      />
-    </nav>
+    <div className={styles.container}>
+      <nav>
+        <MobileMenu isOpen={isOpen} handleClickMenu={handleClickMenu} />
+        <Menu
+          isOpen={isOpen}
+          handleClickMenu={handleClickMenu}
+          isLogged={isLogged}
+          setIsLogged={setIsLogged}
+        />
+      </nav>
+    </div>
   );
 }
